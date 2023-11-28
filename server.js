872490bac64a09f2fa05da2c896c38c0154cb957
Laura Cardoso <lauracardosoy@gmail.com>
2023-11-28 14:46:28 +0100

    link para o css e img

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require(`express`)
 
 const app = express()
 
+ap.use(express.static(`public`))
+app.use(`/css`, express.static(__dirname + `public/css`))
+app.use(`/img`, express.static(__dirname + `public/img`))
 
 app.set(`view engine`, `ejs`)
 
